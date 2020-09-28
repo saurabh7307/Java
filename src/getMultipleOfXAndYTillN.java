57a3d26e85eba201040502ac06ecc56dbfa75c94
saurabh <sau7307@gmail.com>
2020-09-28 16:27:17 +0530

    get the value of x, y and n from user

--- src/getMultipleOfXAndYTillN.java
+++ src/getMultipleOfXAndYTillN.java
@@ -26,7 +26,7 @@ public class getMultipleOfXAndYTillN {
 
     public static void main(String[] var0) {
         Scanner scanner = new Scanner(System.in); // System.in is a standard input stream
-        System.out.println("Enter three value of x, y and n. To get multiple of x and y and till n : ");
+        System.out.println("Enter three value of x, y and n. To get multiple of x and y and till n : like 3 5 1000");
         int firstValue = scanner.nextInt();
         int secondValue = scanner.nextInt();
         int thirdValue = scanner.nextInt();
